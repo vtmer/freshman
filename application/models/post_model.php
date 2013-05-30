62922688f77344892a1121d85db22b41b0493c3f
hbc <bcxxxxxx@gmail.com>
2013-05-30 13:28:31 +0800

    added simple post model

---
+++ application/models/post_model.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Post_model extends CI_Model {
+    function validate($author_id, $content, $title, $source) {
+        if ($content === '' || $title === '')
+            return false;
+
+        $query = $this->db
+            ->get_where('users', array('id' => $author_id), 1);
+        if (!$query->result())
+            return false;
+
+        return true;
+    }
+
+    function create($author_id, $content, $title, $source) {
+        $this->db->insert('posts', array(
+            'author_id' => $author_id,
+            'content' => $content,
+            'title' => $title,
+            'source' => $source
+        ));
+        return $this->db->insert_id();
+    }
+}
