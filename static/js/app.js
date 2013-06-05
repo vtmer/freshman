edc096203382b984797c336124c12e4c559136f1
hbc <bcxxxxxx@gmail.com>
2013-06-05 21:36:24 +0800

    refactoried modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,303 +1,222 @@
 (function() {
-    var basic_uri, set_error, remove_errors;
-
-    basic_uri = (function() {
-        var current = location.href,
-            index_script = 'index.php',
-            index_scriptr = /index\.php/;
-
-        return current.split(index_scriptr)[0] + index_script;
-    })();
-
-    set_error = function(obj) {
-        $(obj).addClass('error');
-    };
-    remove_errors = function() {
+    // 清除所有错误
+    var remove_errors = function() {
         $('.error').removeClass('error');
     };
 
-    // personal info 模块
-    (function() {
-        var modal, inpDispName, inpPassword,
-            oldDispName, oldPassword;
+    // 网站根地址
+    // e.g.
+    //      http://exmaple.com/index.php
+    //      http://exmaple.com/subpath/index.php
+    var basic_uri = (function() {
+        var index_script = 'index.php',
+            index_scriptr = /index\.php/;
 
-        modal = $('#personal-info-modal');
-        inpDispName = $('#personal-info-modal input[name="display_name"]');
-        inpPassword = $('#personal-info-modal input[name="password"]');
-        oldDispName = inpDispName.val();
-        oldPassword = inpPassword.val();
+        return location.href.split(index_scriptr)[0] + index_script;
+    })();
 
-        $('#personal-info-modal a.submit').click(function(e) {
-            var dispName, password,
-                data = {};
-        
-            remove_errors();
-        
-            dispName = inpDispName.val();
-            password = inpPassword.val();
-            
-            if (dispName !== oldDispName)
-                data.display_name = dispName;
-            if (password !== oldPassword)
-                data.password = password;
+    // Input 类
+    // 提供 modal 下表单 input 的逻辑支持
+    // 
+    // @set_error   设置错误
+    // @validate    验证元素
+    // @grab        获取元素值
 
-            if ($.isEmptyObject(data)) {
-                modal.modal('hide');
-                return;
+    // @constructor
+    //
+    // selector     选择器路径
+    // context      父级元素选择器
+    var Input = function(selector, context) {
+        this.input = $(selector, context);
+        this.name = this.input.attr('name');
+    };
+    Input.prototype.set_error = function() {
+        this.input.addClass('error');
+        return this;
+    };
+    Input.prototype.validate = function() {
+        if (!this.grab()) {
+            return false;
+        }
+        return true;
+    };
+    Input.prototype.grab = function() {
+        return this.input.val();
+    };
+
+    // Modal 类
+    // 提供 Modal 组件逻辑支持
+    //
+    // @get_input   获取子 input 组件
+    // @validate    验证子 input 组件
+    // @grab        获取组件数据
+    // @send        发送数据
+
+    // @constructor
+    //
+    // uri          数据保存地址
+    // name         modal 名字
+    // inputs       子 input 组件名字
+    var Modal = function(uri, name, inputs) {
+        var _modal, _inputs;
+
+        this.uri = basic_uri + uri;
+
+        _modal = $('#' + name);
+        this.modal = _modal;
+        this.context = this.modal;
+
+        _inputs = [];
+        inputs.forEach(function(name, idx, arr) {
+            _inputs.push(new Input('[name=' + name + ']', _modal));
+        });
+        this.inputs = _inputs;
+    };
+    Modal.prototype.get_input = function(name) {
+        var input = null;
+
+        this.inputs.forEach(function(value, idx, arr) {
+            if (value.name === name)
+                input = value;
+        });
+        return input;
+    };
+    Modal.prototype.validate = function() {
+        var flag = true;
+
+        remove_errors();
+        this.inputs.forEach(function(value, idx, arr) {
+            if (!value.validate()) {
+                flag = false;
+                value.set_error();
             }
+        }, this);
 
-            /* FIXME better url path */
-            $.post(basic_uri + '/backend/self/update', data).success(function(resp) {
-                modal.modal('hide');
-                location.reload();
-            }).error(function(resp) {
-                var err = resp.responseJSON;
+        return flag;
+    };
+    Modal.prototype.grab = function() {
+        var data = {}, inputs, i;
 
-                if (err.error === 'display_name') {
-                    set_error(inpDispName);
-                }
-                if (err.error === 'password') {
-                    set_error(inpPassword);
+        if (!this.validate())
+            return null;
+
+        this.inputs.forEach(function(value, idx, arr) {
+            data[value.name] = value.grab();
+        });
+        return data;
+    };
+    Modal.prototype.send = function() {
+        var payload = this.grab();
+
+        if (!payload)
+            return;
+        $.post(this.uri, payload)
+            .success(this.send_success())
+            .error(this.send_error());
+    };
+    Modal.prototype.send_success = function() {
+        var that = this;
+
+        return function(resp) {
+            that.modal.modal('hide');
+            location.reload();
+        };
+    };
+    Modal.prototype.send_error = function() {
+        var that = this;
+
+        return function(resp) {
+            var err = resp.responseJSON;
+
+            if (!err || !err.error)
+                return;
+
+            that.inputs.forEach(function(value, idx, arr) {
+                if (value.name === err.error) {
+                    value.set_error();
                 }
             });
+        };
+    };
 
-            e.preventDefault();
-        });
-    })();
+    $('[role="button"]').click(remove_errors);
+
+    // personal info 模块
+    var personal = new Modal('/backend/self/update', 'personal-info-modal',
+                             ['display_name', 'password']);
+    personal.get_input('password').validate = function() { return true; };
+    $('a.submit', personal.context).click(function(e) {
+        e.preventDefault();
+        personal.send();
+    });
 
     // create user 模块
-    (function() {
-        var modal, roles,
-            inpLogName, inpDispName, inpPassword, inpRole,
-            data;
-
-        modal = $('#create-user-modal');
-        roles = $('#create-user-modal select[name="roles"]');
-        inpLogName = $('#create-user-modal input[name="login_name"]');
-        inpDispName = $('#create-user-modal input[name="display_name"]');
-        inpPassword = $('#create-user-modal input[name="password"]');
-        inpRole = roles;
-
-        $('#create-user-modal a.submit').click(function(e) {
-            var ok = true;
-        
-            remove_errors();
-
-            if (!inpLogName.val()) {
-                set_error(inpLogName);
-                ok = false;
-            }
-            if (!inpDispName.val()) {
-                set_error(inpDispName);
-                ok = false;
-            }
-            if (!inpPassword.val()) {
-                set_error(inpPassword);
-                ok = false;
-            }
-            if (!inpRole.val()) {
-                set_error(inpRole);
-                ok = false;
-            }
-
-            if (!ok) {
-                e.preventDefault();
-                return;
-            }
-
-            data = {
-                'login_name': inpLogName.val(),
-                'display_name': inpDispName.val(),
-                'password': inpPassword.val(),
-                'roles': inpRole.val()
-            };
-            $.post(basic_uri + '/backend/user/create', data).success(function(resp) {
-                modal.modal('hide');
-                location.reload();
-            }).error(function(resp) {
-                var err = resp.responseJSON;
-
-                if (err.error === 'login_name')
-                    set_error(inpLogName);
-                if (err.error === 'display_name')
-                    set_error(inpDispName);
-            });
-
-            e.preventDefault();
-        });
-    })();
+    var user_create = new Modal('/backend/user/create', 'create-user-modal',
+                                ['login_name', 'display_name', 'password', 'roles']);
+    $('a.submit', user_create.context).click(function(e) {
+        e.preventDefault();
+        user_create.send();
+    });
 
     // edit user 模块
-    (function() {
-        $('.edit-user select[name="roles"]').each(function(k, v) {
-            var selected = [];
+    $('.edit-user select[name="roles"]').each(function(k, v) {
+        var selected = [];
 
-            $('option[checked]', $(v)).each(function(key, value) {
-                selected.push($(value).attr('value'));
-            });
-
-            $(v).val(selected);
+        $('option[checked]', $(v)).each(function(key, value) {
+            selected.push($(value).attr('value'));
         });
 
-        $('.edit-user a.submit').click(function(e) {
-            var user_id = $(this).attr('data-user-id'),
-                modal, inpLogName, inpDispName, inpPassword, inpRole,
-                ok = true;
+        $(v).val(selected);
+    });
+    $('.edit-user a.submit').click(function (e) {
+        var user_id = $(this).attr('data-user-id'),
+            modal;
 
-            modal = $('#edit-user-modal-' + user_id);
-            inpLogName = $('input[name="login_name"]', modal);
-            inpDispName = $('input[name="display_name"]', modal);
-            inpPassword = $('input[name="password"]', modal);
-            inpRole = $('select[name="roles"]', modal);
-        
-            remove_errors();
+        e.preventDefault();
 
-            if (!inpLogName.val()) {
-                set_error(inpLogName);
-                ok = false;
-            }
-            if (!inpDispName.val()) {
-                set_error(inpDispName);
-                ok = false;
-            }
-            if (!inpRole.val()) {
-                set_error(inpRole);
-                ok = false;
-            }
-
-            if (!ok) {
-                e.preventDefault();
-                return;
-            }
-            data = {
-                'login_name': inpLogName.val(),
-                'display_name': inpDispName.val(),
-                'roles': inpRole.val(),
-                'password': inpPassword.val()
-            };
-
-            $.post(basic_uri + '/backend/user/' + user_id + '/update', data)
-                .success(function(resp) {
-                    modal.modal('hide');
-                    location.reload();
-                }).error(function(resp) {
-                    var err = resp.responseJSON;
-
-                    if (err.error === 'login_name') {
-                        set_error(inpLogName);
-                    }
-                    if (err.error === 'display_name') {
-                        set_error(inpDispName);
-                    }
-                    if (err.error === 'password') {
-                        set_error(inpPassword);
-                    }
-                    if (err.error === 'roles')
-                        set_error(inpRole);
-                });
-
-            e.preventDefault();
-        });
-
-    })();
+        modal = new Modal('/backend/user/' + user_id + '/update',
+                          'edit-user-modal-' + user_id,
+                          ['login_name', 'display_name', 'password', 'roles']);
+        modal.get_input('password').validate = function() { return true; };
+        modal.send();
+    });
 
     // create category 模块
-    (function() {
-        var modal, inpName;
-
-        modal = $('#create-category-modal');
-        inpName = $('input[name="name"]', modal);
-
-        $('a.submit', modal).click(function(e) {
-            var data, ok = true;
-
-            remove_errors();
-
-            if (!inpName.val()) {
-                set_error(inpName);
-                ok = false;
-            }
-            
-            if (!ok) {
-                e.preventDefault();
-                return;
-            }
-
-            data = {
-                name: inpName.val()
-            };
-
-            $.post(basic_uri + '/backend/category/create', data).success(function(resp) {
-                modal.modal('hide');
-                location.reload();
-            }).error(function(resp) {
-                var err = resp.responseJSON;
-
-                if (err.error === 'name')
-                    set_error(inpName);
-
-                e.preventDefault();
-            });
-        });
-    })();
+    create_modal = new Modal('/backend/category/create', 'create-category-modal',
+                             ['name']);
+    $('a.submit', create_modal.context).click(function(e) {
+        e.preventDefault();
+        create_modal.send();
+    });
 
     // edit category 模块
-    (function () {
-        $('.edit-category a.submit').click(function(e) {
-            var cate_id = $(this).attr('data-category-id'),
-                modal, inpName, ok = true, data;
+    $('.edit-category a.submit').click(function(e) {
+        var cate_id = $(this).attr('data-category-id'),
+            modal;
 
-            modal = $('#edit-category-modal-' + cate_id);
-            inpName = $('input[name="name"]', modal);
+        e.preventDefault();
 
-            remove_errors();
+        modal = new Modal('/backend/category/' + cate_id + '/update',
+                          'edit-category-modal-' + cate_id,
+                          ['name']);
+        modal.send();
+    });
+    $('.edit-category a.remove').click(function(e) {
+        var cate_id = $(this).attr('data-category-id');
 
-            if (!inpName.val()) {
-                set_error(inpName);
-                ok = false;
-            }
+        e.preventDefault();
 
-            if (!ok) {
-                e.preventDefault();
-                return;
-            }
-            data = {
-                'name': inpName.val()
-            };
+        $.post(basic_uri + '/backend/category/' + cate_id + '/remove')
+            .success(function(resp) {
+                location.reload();
+            }).error(function(resp) {
+                console.log(resp);
+            });
+    });
 
-            $.post(basic_uri + '/backend/category/' + cate_id + '/update', data)
-                .success(function(resp) {
-                    modal.modal('hide');
-                    location.reload();
-                }).error(function(resp) {
-                    console.log(resp);
-                    var err = resp.responseJSON;
-
-                    if (err.error === 'name') {
-                        set_error(inpName);
-                    }
-                });
-
-            e.preventDefault();
-        });
-
-        $('.edit-category a.remove').click(function(e) {
-            var cate_id = $(this).attr('data-category-id');
-
-            $.post(basic_uri + '/backend/category/' + cate_id + '/remove')
-                .success(function(resp) {
-                    location.reload();
-                }).error(function(resp) {
-                    console.log(resp);
-                });
-
-            e.preventDefault();
-        });
-    })();
-
+    // select2 设置
     $('select[name="roles"]').select2({
         placeholder: "设定用户的角色",
         allowClear: true
     });
-    $('[role="button"]').click(remove_errors);
 })();
