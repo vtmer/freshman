62583f736fdf4100a92b85d03db886bfa9b492c3
hbc <bcxxxxxx@gmail.com>
2013-06-08 22:22:25 +0800

    fixed db default charset

--- schema.sql
+++ schema.sql
@@ -19,29 +19,29 @@ CREATE TABLE `fm_users` (
         `active` TINYINT(1) NOT NULL DEFAULT '1',
         UNIQUE KEY `login_name` (`login_name`),
         UNIQUE KEY `display_name` (`display_name`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_roles` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`name` varchar(255),
         UNIQUE KEY `name` (`name`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_permissions` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`pattern` varchar(255),
         UNIQUE KEY `pattern` (`pattern`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_users_roles` (
 	`user_id` int,
 	`role_id` int,
 	FOREIGN KEY(`user_id`) REFERENCES `fm_users` (`id`),
 	FOREIGN KEY(`role_id`) REFERENCES `fm_roles` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_roles_permissions` (
 	`role_id` int,
 	`permission_id` int,
 	FOREIGN KEY(`role_id`) REFERENCES `fm_roles` (`id`),
 	FOREIGN KEY(`permission_id`) REFERENCES `fm_permissions` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_posts` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`title` varchar(500) NOT NULL,
@@ -51,47 +51,47 @@ CREATE TABLE `fm_posts` (
 	`created_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`status` int NOT NULL DEFAULT '0',
 	FOREIGN KEY(`author_id`) REFERENCES `fm_users` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_tags` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
         UNIQUE KEY `name` (`name`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_posts_tags` (
 	`post_id` int,
 	`tag_id` int,
 	FOREIGN KEY(`post_id`) REFERENCES `fm_posts` (`id`),
 	FOREIGN KEY(`tag_id`) REFERENCES `fm_tags` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_categories` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
         UNIQUE KEY `name` (`name`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_posts_categories` (
 	`post_id` int,
 	`category_id` int,
 	FOREIGN KEY(`post_id`) REFERENCES `fm_posts` (`id`),
 	FOREIGN KEY(`category_id`) REFERENCES `fm_categories` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_post_metas` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`key` varchar(1000) NOT NULL,
 	`value` varchar(1000) NOT NULL,
 	`post_id` int,
 	FOREIGN KEY(`post_id`) REFERENCES `fm_posts` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_user_metas` (
 	`id` int PRIMARY KEY,
 	`key` varchar(1000),
 	`value` varchar(1000),
 	`user_id` int,
 	FOREIGN KEY(`user_id`) REFERENCES `fm_users` (`id`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `fm_sessions` (
 	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	`key` varchar(64) NOT NULL,
 	`user_id` int,
 	FOREIGN KEY(`user_id`) REFERENCES `fm_users` (`id`),
         UNIQUE KEY `key` (`key`)
-);
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
