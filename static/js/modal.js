8021daae4fe80376f2d1c27257f13e398041ffc9
hbc <bcxxxxxx@gmail.com>
2013-06-08 21:56:00 +0800

    fixed base_uri

--- static/js/modal.js
+++ static/js/modal.js
@@ -9,10 +9,17 @@
     //      http://exmaple.com/index.php
     //      http://exmaple.com/subpath/index.php
     var basic_uri = (function() {
-        var index_script = 'index.php',
-            index_scriptr = /index\.php/;
+        var index_script = 'index.php';
 
-        return location.href.split(index_scriptr)[0] + index_script;
+        if (location.href.lastIndexOf(index_script) !== -1) {
+            return location.href.split(index_script)[0] + index_script;
+        } else {
+            // FIXME
+            // 默认使用根地址
+            //
+            // 应该从 config/config.php 的配置来确定？
+            return location.origin;
+        }
     })();
 
     // Input 类
