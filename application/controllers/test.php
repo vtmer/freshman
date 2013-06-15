4dba2048fa1c4c1bae3e40fe13833f6a24229481
hbc <bcxxxxxx@gmail.com>
2013-06-15 21:40:48 +0800

    added test data

---
+++ application/controllers/test.php
@@ -0,0 +1,97 @@
+<?php if (!defined('BASEPATH')) exit ('No direct script access allowed');
+
+/*
+ * freshman
+ *
+ * 新生网
+ *
+ * @author     hbc
+ */
+
+require_once(VENDORPATH . 'autoload.php');
+require_once('backend/auth.php');
+
+// 导入测试数据
+class Test extends Auth_Controller {
+    private $faker;
+    private $category_count = 5;
+    private $tag_count = 15;
+    private $author_count = 5;
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->faker = Faker\Factory::create();
+
+        $this->load->model('post_model');
+        $this->load->model('tag_model');
+        $this->load->model('site_metas_model');
+        $this->load->model('user_model');
+
+        $this->post_model->db
+            ->empty_table('posts_categories');
+        $this->post_model->db
+            ->empty_table('posts_tags');
+        $this->post_model->db
+            ->empty_table('post_metas');
+        $this->post_model->db
+            ->empty_table('posts');
+        $this->user_model->db
+            ->where('user_id > ', 1)
+            ->delete('users_roles');
+        $this->user_model->db
+            ->where('id > ', 1)
+            ->delete('users');
+    }
+
+    public function index() {
+        echo 'start';
+
+        $this->create_users($this->author_count);
+        $this->create_tags($this->tag_count);
+        $this->create_posts();
+
+        echo 'ok';
+    }
+
+    private function create_posts($limit = 50, $tags_limit = 3) {
+        for ($i = 0;$i < $limit;$i += 1) {
+            $post = $this->post_model->create(array(
+                'title' => $this->faker->text(15),
+                'content' => $this->faker->text(2500),
+                'status' => 1,
+                'author_id' => 1
+            ));
+            $this->post_model->update_categories($post->id,
+                [rand() % $this->category_count + 1]);
+
+            $tags = array();
+            for ($j = 0;$j < $tags_limit;$j += 1) {
+                $tags[] = $this->tag_model
+                    ->get_by_id(rand() % $this->tag_count + 1)[0]->name;
+            }
+            $this->post_model->update_tags($post->id, $tags);
+
+            $campus = array();
+            foreach ($this->site_metas_model->get('campus') as $c) {
+                $campus[] = $c->value;
+            }
+            $this->post_model->update_campus($post->id,
+                [$campus[rand() % sizeof($campus)]]);
+        }
+    }
+
+    private function create_tags($limit = 15) {
+        for ($i = 0;$i < $limit;$i += 1) {
+            $this->tag_model->create($this->faker->text(5));
+        }
+        return $limit;
+    }
+
+    private function create_users($limit = 5) {
+        for ($i = 0;$i < $limit;$i += 1) {
+            $this->user_model->create($this->faker->name, $this->faker->name,
+                                      '123', [2]);
+        }
+    }
+}
