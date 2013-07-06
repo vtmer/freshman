d82738dab41495923b3546cafddd7516c12891a3
hbc <bcxxxxxx@gmail.com>
2013-07-06 16:58:22 +0800

    used one-line script

--- static/js/egg.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    if (!window.console) { window.console = {}; }
-    if (!window.console.log) { window.console.log = function() {}; }
-
-    console.log('Hello, world');
-    console.log('对我们感兴趣？一起来玩吧！ =)');
-    console.log('http://www.vtmer.com');
-})();
