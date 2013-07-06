0513ca79297c91ee77dd28d4ddb37478c0559b29
hbc <bcxxxxxx@gmail.com>
2013-07-06 16:40:28 +0800

    added pagination

--- application/controllers/front/category.php
+++ application/controllers/front/category.php
@@ -14,6 +14,37 @@ require_once 'skel.php';
  * 分类控制器
  */
 class Category extends Skel {
+    // 分页配置
+    protected $pages = array(
+        'base_url' => '',
+        'total_rows' => 0,
+        'query_string_segment' => 'p',
+        'per_page' => 15,
+        'user_page_numbers' => false,
+        'page_query_string' => true,
+
+        'first_link' => '首页',
+        'first_tag_open' => '<li>',
+        'first_tag_close' => '</li>',
+
+        'last_link' => '尾页',
+        'last_tag_open' => '<li>',
+        'last_tag_close' => '</li>',
+
+        'next_link' => '>',
+        'next_tag_open' => '<li>',
+        'next_tag_close' => '</li>',
+
+        'prev_link' => '<',
+        'prev_tag_open' => '<li>',
+        'prev_tag_close' => '</li>',
+
+        'cur_tag_open' => '<li><a>',
+        'cur_tag_close' => '</a></li>',
+        'num_tag_open' => '<li>',
+        'num_tag_close' => '</li>'
+    );
+
     public function __construct() {
         parent::__construct();
         
@@ -23,9 +54,13 @@ class Category extends Skel {
 
     /*
      * /c/(:num)
-     * TODO 分页
+     * 
+     * 分页:
+     *  p = 第 n 页
      */
     public function category($cate_id) {
+        $config = $this->pages;
+        $p = intval($this->input->get($config['query_string_segment']));
         $category = $this->category_model->get_by_id($cate_id);
         if (!$category) {
             // FIXME  统一的 404 页面
@@ -34,31 +69,56 @@ class Category extends Skel {
         } else {
             $category = $category[0];
         }
-        $category->posts = $this->posts($cate_id, $this->visitor['campus']);
+        $category->posts = $this->posts(
+            $cate_id, $this->visitor['campus'],
+            $config['per_page'], $p / $config['per_page']
+        );
+
+        $this->load->library('pagination');
+        $this->load->helper('url');
+        $config['base_url'] = site_url('/c/' . $cate_id);
+        $config['total_rows'] = $this
+            ->posts_count($cate_id, $this->visitor['campus']);
+        $this->pagination->initialize($config);
 
         $this->display('front/list.html', array(
-            'category' => $category
+            'category' => $category,
+            'pagination' => $this->pagination->create_links()
         ));
     }
 
     /* 
      * /c/all
      * 所有分类页面
-     *TODO 分页
+     * 
+     * 分页:
+     *  p = 第 n 页
      */
     public function all() {
+        $config = $this->pages;
+        $p = intval($this->input->get($config['query_string_segment']));
         $category = array(
             'name' => '所有文章',
-            'posts' => $this->all_posts($this->visitor['campus'])
+            'posts' => $this->all_posts(
+                $this->visitor['campus'],
+                $config['per_page'],
+                $p / $config['per_page'])
         );
 
+        $this->load->library('pagination');
+        $this->load->helper('url');
+        $config['base_url'] = site_url('/c/all');
+        $config['total_rows'] = $this->all_posts_count($this->visitor['campus']);
+        $this->pagination->initialize($config);
+
         $this->display('front/list.html', array(
-            'category' => $category
+            'category' => $category,
+            'pagination' => $this->pagination->create_links()
         ));
     }
 
     // TODO 更好的封装
-    private function posts($cate_id, $campus) {
+    private function posts($cate_id, $campus, $limit, $page) {
         return $this->post_model->pack_posts(
             $this->post_model->db
                 ->select('posts.*')
@@ -68,13 +128,28 @@ class Category extends Skel {
                 ->where('post_metas.value', $campus)
                 ->where('posts.status', 1)
                 ->where('posts_categories.category_id', $cate_id)
+                ->distinct()
+                ->limit($limit, $page * $limit)
                 ->get('posts')
                 ->result()
         );
     }
 
+    private function posts_count($cate_id, $campus) {
+        return sizeof($this->post_model->db
+            ->select('posts.*')
+            ->join('posts_categories', 'posts_categories.post_id = posts.id')
+            ->join('post_metas', 'post_metas.post_id = posts.id')
+            ->where('post_metas.key', 'campus')
+            ->where('post_metas.value', $campus)
+            ->where('posts.status', 1)
+            ->where('posts_categories.category_id', $cate_id)
+            ->get('posts')
+            ->result());
+    }
+
     // TODO 更好的封装
-    private function all_posts($campus) {
+    private function all_posts($campus, $limit, $page) {
         return $this->post_model->pack_posts(
             $this->post_model->db
                 ->select('posts.*')
@@ -82,8 +157,22 @@ class Category extends Skel {
                 ->where('post_metas.key', 'campus')
                 ->where('post_metas.value', $campus)
                 ->where('posts.status', 1)
+                ->distinct()
+                ->limit($limit, $page * $limit)
                 ->get('posts')
                 ->result()
         );
+    }
+
+    private function all_posts_count($campus) {
+        return sizeof($this->post_model->db
+            ->select('posts.*')
+            ->join('post_metas', 'post_metas.post_id = posts.id')
+            ->where('post_metas.key', 'campus')
+            ->where('post_metas.value', $campus)
+            ->where('posts.status', 1)
+            ->distinct()
+            ->get('posts')
+            ->result());
     }
 }
