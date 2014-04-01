054cd20c35900397091fe34a81260443354582bc
Yannyezixin <646840080@qq.com>
2014-04-01 23:54:44 +0800

    add the form to saveartical

--- backend/app/views/Backend/Artical/Artical_part.blade.php
+++ backend/app/views/Backend/Artical/Artical_part.blade.php
@@ -22,11 +22,13 @@
             <th>状态 | 文章名</th>
             <th>作者</th>
             <th>分类</th>
+            <th>浏览数</th>
             <th>发表日期</th>
             <th>操作</th>
               </tr>
             </thead>
             <tbody>
+        @if(!empty($articals))
 	    @foreach($articals as $artical)
 	     <tr>
 		<td><a href=""><span class="glyphicon glyphicon-ok"></span></a> <a href="">{{ $artical['title'] }}</a></td>
@@ -35,10 +37,18 @@
 			<span class="label label-success">{{ $catagory['catagory'] }}</span>
 		    @endforeach
 		</td>
+        <td>{{ $artical['see']}}</td>
 		<td>{{ $artical['created_at'] }}</td>
-		<td><a href=""><span class="glyphicon glyphicon-pencil"></span></a> | <a href="{{{ URL::route('BackendRemoveArtical',$artical['id'])}}}" onclick="return confirm('亲～～，你确定要删除么？')"><span class="glyphicon glyphicon-trash"></span></a> | <a href=""><span class="glyphicon glyphicon-thumbs-down"></span></a></td>
+        <td><a href=""><span class="glyphicon glyphicon-pencil"></span></a> |
+            <a href="{{{ URL::route('BackendRemoveArtical',$artical['id'])}}}" onclick="return confirm('亲～～，你确定要删除么？')"><span class="glyphicon glyphicon-trash"></span></a> |
+            <a href="{{{ URL::route('BackendUpdownArtical',$artical['id'])}}}" title="置顶">
+            @if($artical['updown']=='0')
+                <span class="glyphicon glyphicon-thumbs-down"></span>
+            @else<span class="glyphicon glyphicon-thumbs-up"></a></td>
+            @endif
 	     </tr>
 	   @endforeach
+       @endif
 
             </tbody>
           </table>
