62340193f18ad36db617af48b07d9b83e7ec04f5
Yannyezixin <646840080@qq.com>
2014-06-07 16:21:51 +0800

    列表页数据绑定

---
+++ backend/app/views/Front/Modules/Sidebar.blade.php
@@ -0,0 +1,63 @@
+<div class="sidebar">
+    <?php $j =1 ?>
+    @foreach($catagoriesList as $catagory)
+    @if( $j != 4)
+	@if($catagory['id'] !== $chooseCatagoryId)
+			<div class="form">
+				<h1 class="title">
+					{{ $catagory['catagory'] }}
+					<span  class="special_more" ><a href="{{{ URL::route('FrontendListByCatagoryId',$catagory['id']) }}}">MORE</a></span>
+				</h1>
+					<ul class="list_box">
+                    @if(isset($catagory['articles'][0]))
+                    <li><span class="first">1</span>
+							<a href="">{{ $catagory['articles'][0]['title'] }}</a>
+						  </li>
+                    @endif
+                    @if(isset($catagory['articles'][1]))
+					<li><span class="second">2</span>
+							<a href="">{{ $catagory['articles'][1]['title'] }}</a>
+						  </li>
+                    @endif
+                    @if(isset($catagory['articles'][2]))
+					<li><span class="third">3</span>
+							<a href="">{{ $catagory['articles'][2]['title'] }}</a>
+						  </li>
+                    @endif
+                    @if(isset($catagory['articles'][3]))
+					<li id="lineNone"><span>&nbsp;</span>
+							<a href="">{{ $catagory['articles'][3]['title'] }}</a>
+						  </li>
+                    @endif
+					</ul>
+			</div>
+            <br />
+	@endif
+    @endif
+    <?php $j++; ?>
+    @endforeach
+
+    @if($catagoriesList[3]['id'] !== $chooseCatagoryId)
+			<div class="form_box">
+				<h1>
+                    {{ $catagoriesList[3]['catagory']}}&nbsp;HOT&nbsp;NEWS<span  class="special_more">
+                            <a href="{{{ URL::route('FrontendListByCatagoryId',$catagoriesList[3]['id'])}}}">MORE</a></span>
+				</h1>
+				<ul class="list_box">
+                    <?php $i = 1; ?>
+                    @foreach($catagoriesList[3]['articles'] as $article)
+                    @if($i == 1)<li id="list_boxFirst"><span class="first">1</span>
+                    @elseif($i == 2)<li><span class="second">2</span>
+                    @elseif($i == 3)<li><span class="third">3</span>
+                    @else <li><span>&nbsp;</span>
+                    @endif
+						<a href="">{{ $article['title']}}</a>
+						<p>发布时间：{{ $article['created_at']}}</p>
+					</li>
+                    <?php $i++; ?>
+                    @endforeach
+				</ul>
+			</div>
+        @endif
+		</div>
+
