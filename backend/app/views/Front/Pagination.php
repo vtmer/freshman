1bef2bf6940a27122cb4655334e4f928d13972b0
Yannyezixin <646840080@qq.com>
2014-07-12 22:54:12 +0800

    下一页上一页按钮

--- backend/app/views/Front/Pagination.php
+++ backend/app/views/Front/Pagination.php
@@ -1,16 +1,28 @@
-
 <?php
 	$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator);
-
-	$trans = $environment->getTranslator();
 ?>
 
 <?php if ($paginator->getLastPage() > 1): ?>
         <ul class="pagination">
-		<?php
-			echo $presenter->getPrevious($trans->trans('上一页'));
-
-			echo $presenter->getNext($trans->trans('下一页'));
-		?>
+            <?php echo getPrevious($paginator->getCurrentPage(), $paginator->getUrl( $paginator->getCurrentPage()-1 ) ) ?>
+            <?php echo getNext($paginator->getCurrentPage(), $paginator->getLastPage(), $paginator->getUrl( $paginator->getCurrentPage()+1 ) )  ?>
         </ul>
 <?php endif; ?>
+
+<?php
+function getPrevious($currentPage, $url)
+{
+    if ($currentPage <= 1)
+        return '<li><a>上一页</a></li>';
+    else
+       return '<li><a href="'.$url.'">上一页</a></li>';
+}
+
+function getNext($currentPage, $lastPage, $url)
+{
+    if ($currentPage >= $lastPage)
+        return '<li><a>下一页</a></li>';
+    else
+        return '<li><a href="'.$url.'">下一页</a></li>';
+}
+?>
