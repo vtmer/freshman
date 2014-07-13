abd64bf765d63bec27dc7dfb8840f799ff4600eb
Yannyezixin <646840080@qq.com>
2014-07-13 09:36:02 +0800

    移除嵌在view里的函数

--- backend/app/views/Front/Pagination.php
+++ backend/app/views/Front/Pagination.php
@@ -4,25 +4,8 @@
 
 <?php if ($paginator->getLastPage() > 1): ?>
         <ul class="pagination">
-            <?php echo getPrevious($paginator->getCurrentPage(), $paginator->getUrl( $paginator->getCurrentPage()-1 ) ) ?>
-            <?php echo getNext($paginator->getCurrentPage(), $paginator->getLastPage(), $paginator->getUrl( $paginator->getCurrentPage()+1 ) )  ?>
+            <?php echo getPage::getPrevious($paginator->getCurrentPage(), $paginator->getUrl( $paginator->getCurrentPage()-1 ) ) ?>
+            <?php echo getPage::getNext($paginator->getCurrentPage(), $paginator->getLastPage(), $paginator->getUrl( $paginator->getCurrentPage()+1 ) )  ?>
         </ul>
 <?php endif; ?>
 
-<?php
-function getPrevious($currentPage, $url)
-{
-    if ($currentPage <= 1)
-        return '<li><a>上一页</a></li>';
-    else
-       return '<li><a href="'.$url.'">上一页</a></li>';
-}
-
-function getNext($currentPage, $lastPage, $url)
-{
-    if ($currentPage >= $lastPage)
-        return '<li><a>下一页</a></li>';
-    else
-        return '<li><a href="'.$url.'">下一页</a></li>';
-}
-?>
