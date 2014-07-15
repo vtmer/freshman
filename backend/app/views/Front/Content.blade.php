a7b5a8eafdc65cee791ec86deca0b70400d1b2e3
Yannyezixin <646840080@qq.com>
2014-07-15 17:20:47 +0800

    添加bShare

--- backend/app/views/Front/Content.blade.php
+++ backend/app/views/Front/Content.blade.php
@@ -27,13 +27,20 @@
                 </div>
                 <p>
                     {{ $article['content']}}
-                </p>
-                <h1 class="nav1">
+                </p><br />
+                <div class="bshare-custom">分享到：
+                    <a title="分享到QQ空间" class="bshare-qzone"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a>
+                    <a title="分享到人人网" class="bshare-renren"></a><a title="分享到腾讯微博" class="bshare-qqmb"></a>
+                    <a title="分享到网易微博" class="bshare-neteasemb"></a><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a>
+                    <span class="BSHARE_COUNT bshare-share-count">0</span>
+                </div>
+                <script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=771c5856-d516-4aff-a0eb-6f7b3680c20c&amp;pophcol=3&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script>
+                <p class="nav1">
                 	<span class="relative">
                 		&nbsp;
                 	</span>
                 	相关文章
-                </h1>
+                </p>
                 <ul class="list">
                 @foreach($catagoriesList as $catagory)
                 @if($catagory['id'] == $currentCatagory['id'])
